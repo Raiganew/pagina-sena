a5a3fc77d2c1c61694d34c406bcd12aba16a98ee
Raiganew <64878789+Raiganew@users.noreply.github.com>
2020-08-05 10:41:57 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1, maximum-scale=1, minimum-scale=1">
+    <title>Inicio</title>
+<link rel="stylesheet" href="estilos.css">
+    <link rel="stylesheet" href="dist/flexboxgrid.min.css">
+</head>
+<body>
+
+
+    <div class="row">
+        <div class="col-lg-4 col-md-6 col-sm-12 col-xs-12">
+          <div class="space2">
+             <h1> Bienvenido a la plataforma de estudiantes</h1>
+             <p> En esta plataforma usted podra ingresar nuevos estudiantes, verlos, editarlos y eliminarlos. Pulse este botón para conocer la tabla de estudiantes registrados en la base de datos</p>
+             
+            <div class="boton-next">
+             <a href="tabla_general.php" ><p>Conocer la tabla</p></a>
+             </div>
+             
+          </div>  
+      </div>
+      <div class="col-lg-4 col-md-6 col-sm-12 col-xs-12">
+          <div class="space3">
+             <h1> Registre un estudiante en la base de datos</h1>
+             <p> Registre a un estudiante o más en la base de datos, rellenando información relevante acerca de el. Podrá acerlo dando click en la opción del menú superior, o dando click al siguiente enlace:</p>
+             <div class="boton-next2">
+             <a href="registro.html" ><p>Registrar estudiante</p></a>
+             </div>
+          </div>  
+      </div>
+      <div class="col-lg-4 col-md-6 col-sm-12 col-xs-12">
+          <div class="space4">
+             <h1> Contacte con los creadores</h1>
+             <p> Contacte con uno de nuestros programadores para obtener ascesoria acerca del funcionamiento de esta sitio. Podrá obtener mmás información dando click en la opción de "Contacto" en la parte superior o dando click al siguiente enlace_</p>
+             <div class="boton-next3">
+             <a href="" ><p>Contactenos ya mismo</p></a>
+             </div>
+          </div>  
+      </div>
+    </div>
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="js/bootstrap.min.js"></script>
+    
+</body>
+</html>
